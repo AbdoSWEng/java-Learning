19d4abb268a53088f8b764032c53d5f7efaaeeb8
AbdoSWEng <itabdo4tf@gmail.com>
2026-08-08 21:08:34 +0200

    Add variables exmple

---
+++ dataVariables/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+   public static void main(String[] var0) {
+      // Declare two variables
+      int i = 10;
+      int x = 20;
+
+      // Display the variables
+      System.out.println(i);
+      System.out.println(x);
+   }
+}
