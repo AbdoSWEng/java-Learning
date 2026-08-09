8ad3b057683c93f76ada3908d191c88afcd84009
AbdoSWEng <itabdo4tf@gmail.com>
2026-08-09 19:14:05 +0200

    add java exercise Temperature

---
+++ 01_variables/Temperature.java
@@ -0,0 +1,14 @@
+public class Temperature {
+    public static void main(String[] args) {
+        // declare and initialize kellar
+        double kellar = 10;
+
+        // convert kellar to pahsat
+        double pahsat = kellar * 3 * 30;
+
+        // Display the results
+        System.out.println("Kellar = " + kellar);
+        System.out.println("Pahsat = " + pahsat);
+
+    }
+}
